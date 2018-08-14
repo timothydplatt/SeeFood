4bda697b220c0500df1fc1166392b7876ef9fb2a
Timothy Platt <timothyplatt@timothys-mbp>
2018-08-14 12:19:57 +0100

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Timothy Platt on 14/08/2018.
+//  Copyright © 2018 Timothy Platt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
