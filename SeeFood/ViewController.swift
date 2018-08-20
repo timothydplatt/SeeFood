b93e8dbc1e43aed8b44986bd2c4cf969a8f17512
Timothy Platt <timothyplatt@timothys-mbp>
2018-08-20 12:34:35 +0100

    Finish SeeFood. Classify image and display result of classification.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -19,21 +19,59 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //imagePicker.sourceType = .camera
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = false
-
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
             imageView.image = userPickedImage
             
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Couldn't convert uiimage to CIImage")
+            }
+            
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hot Dog"
+                } else {
+                    self.navigationItem.title = "Not a Hot Dog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
@@ -41,8 +79,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
         
     }
-    
-    
     
     
 }
